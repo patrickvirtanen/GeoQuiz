9c9a5c1811a7039362a8af9aa61cdd9aa3e98690
patrick <patrick.virtanen@gmail.com>
2018-06-16 18:36:20 +0200

    Created a question class

---
+++ app/src/main/java/com/virtanen/android/geoquiz/Question.java
@@ -0,0 +1,29 @@
+package com.virtanen.android.geoquiz;
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int mTextResId, boolean mAnswerTrue){
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+
+    }
+}
